c7faf1009ae8c5047a5e4daa49476332932c0ad5
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2023-05-18 12:24:40 +0300

    Solve question 1 and this commit is for question 2

---
+++ classes.py
@@ -0,0 +1,35 @@
+class Publication:
+    title: str
+    price: float
+
+    def __init__(self, title: str, price: float):
+        self.title = title
+        self.price = price
+
+
+class Book(Publication):
+    page_count: int
+
+    def put_data(self):
+        print("Title: ", self.title)
+        print("Price: ", self.price)
+        print("Page count: ", self.page_count)
+
+    def get_data(self):
+        self.title = input("Enter title: ")
+        self.price = float(input("Enter price: "))
+        self.page_count = int(input("Enter page count: "))
+
+
+class Tape(Publication):
+    playing_time: float
+
+    def put_data(self):
+        print("Title: ", self.title)
+        print("Price: ", self.price)
+        print("Playing time: ", self.playing_time)
+
+    def get_data(self):
+        self.title = input("Enter title: ")
+        self.price = float(input("Enter price: "))
+        self.playing_time = float(input("Enter playing time: "))
